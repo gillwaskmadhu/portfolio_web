8c095777648300271ffc2951b5b2aba6684bdb35
gillwaskmadhu555 <gilwas.km@iroidtechnologies.com>
2020-08-09 23:08:42 -0700

    first_commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+//compile   SCSS into CSS
+
+function style(){
+    return gulp.src('./scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+
+}
+exports.style = style;
